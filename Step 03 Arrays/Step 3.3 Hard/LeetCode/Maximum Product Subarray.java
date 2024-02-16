104c7f3fb55a23fbfb7e56e80e78b87caa0c6467
arise-aizen-404 <maheshavvaru2004@gmail.com>
2024-02-16 22:06:36 +0530

    updated code

--- Step 03 Arrays/Step 3.3 Hard/LeetCode/Maximum Product Subarray.java
+++ Step 03 Arrays/Step 3.3 Hard/LeetCode/Maximum Product Subarray.java
@@ -1,19 +1,16 @@
 class Solution {
-    public int maxProduct(int[] nums) {
-        int ans = nums[0];
-        int max = nums[0];
-        int min = nums[0];
-        
-        for(int i=1;i<nums.length;i++){
-            if(nums[i]<0){
-                int temp=min;
-                min=max;
-                max=temp;
-            }
-            max=Math.max(nums[i],nums[i]*max);
-            min=Math.min(nums[i],nums[i]*min);
-            ans=Math.max(ans,max);
+    public int maxProduct(int[] arr) {
+        int n = arr.length;
+        int prefix = 1;
+        int suffix = 1;
+        int maxx = Integer.MIN_VALUE;
+        for(int i=0;i<n;++i) {
+            if(prefix == 0) prefix = 1;
+            if(suffix == 0) suffix = 1;
+            prefix *= arr[i];
+            suffix *= arr[n-i-1];
+            maxx = Math.max(maxx, Math.max(prefix, suffix));
         }
-        return ans;
+        return maxx;
     }
 }
