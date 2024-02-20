4588c3e12d6940680361196e093603d5a25fc357
arise-aizen-404 <maheshavvaru2004@gmail.com>
2024-02-20 22:57:30 +0530

    Problem -7

---
+++ Step 04 Binary Search/Step 4.1 Learning BS on 1D Array/LeetCode/count_no_of_occurences_in_sorted_array.java
@@ -0,0 +1,34 @@
+int startPos = -1;
+        int low = 0;
+        int high = n - 1;
+
+        // Finding the start position
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] >= k) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        startPos = low;
+
+        if (startPos >= n || arr[startPos] != k) {
+            return 0;
+        }
+
+        // Finding the end position
+        int endPos = -1;
+        low = 0;
+        high = n - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] > k) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        endPos = high;
+
+        return endPos - startPos + 1;
