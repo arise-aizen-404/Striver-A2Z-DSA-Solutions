17c2f133ac7ed9f51f9a04d5ce8a4cc1e53c63ff
arise-aizen-404 <maheshavvaru2004@gmail.com>
2024-03-04 21:55:27 +0530

    updated problem 2

--- Step 04 Binary Search/Step 4.3 Find Answers by BS in Search Space/LeetCode/Median of Two Sorted Arrays.java
+++ Step 04 Binary Search/Step 4.3 Find Answers by BS in Search Space/LeetCode/Median of Two Sorted Arrays.java
@@ -1,6 +1,7 @@
 class Solution {
-    public double findMedianSortedArrays(int[] arr1, int[] arr2) {
-        int m=arr1.length;
+     public double findMedianSortedArrays(int[] arr1, int[] arr2) {
+         
+         int m=arr1.length;
         int n=arr2.length;
         if(m>n) return findMedianSortedArrays(arr2,arr1);
         
@@ -24,5 +25,32 @@ class Solution {
             else low = cut1+1;
         }
         return 0.0;
+         
+// 		int arr[]=new int[nums1.length + nums2.length];
+
+// 		double ans=0; int index =0;
+// 		  for(int i=0;i<nums1.length;i++){
+// 		      arr[index]=nums1[i];
+// 		      index++;
+// 		  }
+
+// 		  for(int i=0;i<nums2.length;i++){
+// 		      arr[index]=nums2[i];
+// 		      index++;
+// 		  }
+
+// 		Arrays.sort(arr);
+// 		int len=arr.length;
+// 		int a=len/2;
+// 		    if(len%2!=0){
+// 				int val= (len+1)/2;
+// 				ans=arr[val-1];
+// 			}
+// 		    else{
+// 		        double val=(arr[a-1] + arr[a]);
+// 		        val=val/2;
+//                 ans=val;
+// 		    }
+//     return ans;    
     }
 }
