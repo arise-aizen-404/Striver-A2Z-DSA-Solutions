e8f98e6cb87bc483ad11ddf0345573e676fdfaa5
arise-aizen-404 <maheshavvaru2004@gmail.com>
2024-03-04 21:55:21 +0530

    updated problem

--- Step 04 Binary Search/Step 4.3 Find Answers by BS in Search Space/LeetCode/Kth Missing Positive Number.java
+++ Step 04 Binary Search/Step 4.3 Find Answers by BS in Search Space/LeetCode/Kth Missing Positive Number.java
@@ -1,11 +1,19 @@
 class Solution {
     public int findKthPositive(int[] arr, int k) {
-        int l = 0, r = arr.length;
-        while (l < r) {
-            int mid = l + (r - l) / 2;
-            if (arr[mid] - (mid + 1) >= k) r = mid;
-            else l = mid + 1;
+        int low = 0;
+        int high = arr.length - 1;
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+            int missingNums = arr[mid] - (mid + 1);
+            if(missingNums < k) low = mid + 1;
+            else high = mid - 1;
         }
-        return l + k;
+        // formula:
+        // arr[high] + more
+        // arr[high] + k - missingNums;
+        // arr[high] + k - (arr[high] - (high + 1));
+        // high + 1 + k;
+        // low = high + 1 -> where traversing stops in BSearch;
+        return low + k;
     }
 }
