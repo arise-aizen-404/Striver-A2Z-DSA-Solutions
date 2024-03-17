87c14c9e87d87b9c779c2e6e357b55b2955397a6
arise-aizen-404 <maheshavvaru2004@gmail.com>
2024-03-17 11:50:57 +0530

    problem 9

---
+++ Step 06 LinkedList/Step 6.2 Doubly Linked List/LeetCode/Reverse a Doubly Linked List.java
@@ -0,0 +1,51 @@
+/****************************************************************
+
+ Following is the class structure of the Node class:
+
+ class Node {
+     public int data;
+     public Node next;
+     public Node prev;
+
+     Node()
+     {
+         this.data = 0;
+         this.next = null;
+         this.prev = null;
+     }
+
+     Node(int data)
+     {
+         this.data = data;
+         this.next = null;
+         this.prev = null;
+     }
+
+     Node(int data, Node next)
+     {
+         this.data = data;
+         this.next = next;
+         this.prev = next;
+     }
+ };
+
+ *****************************************************************/
+
+public class Solution
+{
+    public static Node reverseDLL(Node head)
+    {
+        // Write your code here.
+        if(head == null || head.next == null)
+            return head;
+        Node curr = head;
+        Node prev = null;
+        while(curr != null) {
+            prev = curr.prev;
+            curr.prev = curr.next;
+            curr.next = prev;
+            curr = curr.prev;
+        }
+        return prev.prev;
+    }
+}
