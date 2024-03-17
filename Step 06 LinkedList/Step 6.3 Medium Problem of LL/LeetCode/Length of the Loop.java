f095543b1e076fa7a7c41eee95ed3d28685ddf45
arise-aizen-404 <maheshavvaru2004@gmail.com>
2024-03-17 11:52:23 +0530

    problem 14

---
+++ Step 06 LinkedList/Step 6.3 Medium Problem of LL/LeetCode/Length of the Loop.java
@@ -0,0 +1,49 @@
+/****************************************************************
+
+ Following is the class structure of the Node class:
+
+ class Node {
+     public int data;
+     public Node next;
+
+     Node()
+     {
+         this.data = 0;
+         this.next = null;
+     }
+
+     Node(int data)
+     {
+         this.data = data;
+         this.next = null;
+     }
+
+     Node(int data, Node next)
+     {
+         this.data = data;
+         this.next = next;
+     }
+ };
+
+ *****************************************************************/
+
+public class Solution
+{
+    public static int lengthOfLoop(Node head) {
+        // Write your code here
+        Node slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) break;
+        }
+        if (fast == null || fast.next == null) return 0;
+        slow = slow.next;
+        int count = 1;
+        while (fast != slow) {
+            count++;
+            slow = slow.next;
+        }
+        return count;
+    }
+}
