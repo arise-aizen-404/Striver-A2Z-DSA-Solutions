6016a1beda6775b422126db94d97a28d94f8bf5a
arise-aizen-404 <maheshavvaru2004@gmail.com>
2024-03-17 11:52:58 +0530

    problem 16

---
+++ Step 06 LinkedList/Step 6.3 Medium Problem of LL/LeetCode/Linked List Cycle.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode rabbit = head;
+        ListNode tortoise = head;
+        while (rabbit != null && rabbit.next != null) {
+            rabbit = rabbit.next.next;
+            tortoise = tortoise.next;
+            if (rabbit == tortoise) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
