a914c2287664ae8b88393bfbabc66371e9042528
arise-aizen-404 <maheshavvaru2004@gmail.com>
2024-03-17 11:55:27 +0530

    problem 25

---
+++ Step 06 LinkedList/Step 6.4 Medium Problem of DLL/LeetCode/Find pairs with given sum in DLL.java
@@ -0,0 +1,42 @@
+import java.util.* ;
+import java.io.*; 
+/****************************************************************
+
+    Following is the class structure of the Node class:
+
+      class Node<T> {
+        T data;
+        Node<T> next;
+        Node<T> prev;
+
+        public Node(T data) {
+            this.data = data;
+        }
+    }
+
+*****************************************************************/
+public class Solution {
+    public static ArrayList<int[]> findPairs(Node head, int k) {
+        // Traverse the linked list to the end.
+        Node tempHead = head;
+        Node tempTail = head;
+        ArrayList<int[]> ans = new ArrayList<>();
+        while(tempTail.next !=null){
+            tempTail = tempTail.next;
+        }
+        while(tempHead != null && tempTail != null && tempHead.data < tempTail.data){
+            int sum = tempHead.data + tempTail.data;
+            if(sum == k){
+                ans.add(new int[]{tempHead.data, tempTail.data});
+                tempHead = tempHead.next;
+                tempTail = tempTail.prev;
+            } else if(sum > k){
+                tempTail = tempTail.prev;
+            } else {
+                tempHead = tempHead.next;
+            }
+        }
+        return ans;
+    }
+
+}
