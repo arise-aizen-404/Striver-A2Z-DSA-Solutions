435ea22e493dcb3b4acfa28055b858052e09b023
0xAriseAizen-404 <maheshavvaru2004@gmail.com>
2026-07-14 23:07:07 +0530

    bst

--- Step 13 Binary Trees/Step 13.2 Medium Problems/199. Binary Tree Right Side View/199. Binary Tree Right Side View.cpp
+++ Step 13 Binary Trees/Step 13.2 Medium Problems/199. Binary Tree Right Side View/199. Binary Tree Right Side View.cpp
@@ -18,13 +18,9 @@ public:
         que.push(root);
         while (!que.empty()) {
             int sz = que.size();
-            int isFoundAtThisLevel = false;
             for (int i=0; i<sz; ++i) {
                 TreeNode *curr = que.front(); que.pop();
-                if (!isFoundAtThisLevel) {
-                    rightViewNodes.push_back(curr->val);
-                    isFoundAtThisLevel = true;
-                }
+                if (i == 0) rightViewNodes.push_back(curr->val);
                 // Right View
                 if (curr->right) que.push(curr->right);
                 if (curr->left) que.push(curr->left);
@@ -36,3 +32,52 @@ public:
         return rightViewNodes;
     }
 };
+
+// class Solution {
+// public:
+//     vector<int> rightSideView(TreeNode* root) {
+//         vector<int> rightViewNodes;
+//         if (!root) return rightViewNodes;
+//         queue<TreeNode *> que;
+//         que.push(root);
+//         while (!que.empty()) {
+//             int sz = que.size();
+//             int isFoundAtThisLevel = false;
+//             for (int i=0; i<sz; ++i) {
+//                 TreeNode *curr = que.front(); que.pop();
+//                 if (!isFoundAtThisLevel) {
+//                     rightViewNodes.push_back(curr->val);
+//                     isFoundAtThisLevel = true;
+//                 }
+//                 // Right View
+//                 if (curr->right) que.push(curr->right);
+//                 if (curr->left) que.push(curr->left);
+//                 // // Left View
+//                 // if (curr->left) que.push(curr->left);
+//                 // if (curr->right) que.push(curr->right);
+//             }
+//         }
+//         return rightViewNodes;
+//     }
+// };
+
+// class Solution {
+// public:
+//     vector<int> rightSideView(TreeNode* root) {
+//         vector<int> rightViewNodes;
+//         if (!root) return rightViewNodes;
+//         queue<TreeNode *> que;
+//         que.push(root);
+//         while (!que.empty()) {
+//             int sz = que.size();
+//             bool firstOne = true;
+//             for (int i=0; i<sz; ++i) {
+//                 TreeNode *curr = que.front(); que.pop();
+//                 if (i == sz - 1) rightViewNodes.push_back(curr->val);
+//                 if (curr->left) que.push(curr->left);
+//                 if (curr->right) que.push(curr->right);
+//             }
+//         }
+//         return rightViewNodes;
+//     }
+// };
